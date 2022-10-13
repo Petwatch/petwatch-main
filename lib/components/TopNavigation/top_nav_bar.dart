1437fbb98e8bbc52ced23e950324a8aa466ae2c9
Roman Fedoruk <rfedoruk@students.kennesaw.edu>
2022-10-13 16:58:00 -0400

    feature: Top Navigation
    Implemented top navigation bar when you are signed in, placeholder for the message screen, nothing for filtering as of now.

---
+++ lib/components/TopNavigation/top_nav_bar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:petwatch/screens/message_screen.dart';
+
+class TopNavBar extends StatefulWidget implements PreferredSizeWidget {
+  const TopNavBar({Key? key}) : super(key: key);
+
+  static final _appBar = AppBar();
+  @override
+  Size get preferredSize => _appBar.preferredSize;
+
+  @override
+  _TopNavBarState createState() => _TopNavBarState();
+}
+
+class _TopNavBarState extends State<TopNavBar> {
+  Color _color = Colors.red;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        color: Colors.white,
+        iconSize: 35,
+        icon: const Icon(Icons.filter_alt_rounded),
+        onPressed: () => {},
+      ),
+      title: Text("Logo"),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      actions: [
+        IconButton(
+          color: Colors.white,
+          icon: const Icon(Icons.message_outlined),
+          iconSize: 35,
+          onPressed: () => {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => MessageScreen()))
+          },
+        )
+      ],
+    );
+  }
+}
--- lib/screens/message_screen.dart
+++ lib/screens/message_screen.dart
@@ -1,26 +1,19 @@
-import 'dart:math';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
+import 'package:petwatch/components/TopNavigation/message_top_nav.dart';
 import 'package:petwatch/screens/auth_gate.dart';
 import 'package:petwatch/screens/sign-up/personal_info.dart';
 import 'package:petwatch/components/TopNavigation/top_nav_bar.dart';
 
-class HomePage extends StatelessWidget {
-  // @override
-  // State<StatefulWidget> createState() {
-  //   // TODO: implement createState
-  //   throw UnimplementedError();
-  // }
-
+class MessageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
         onTap: () {},
         child: Scaffold(
-            appBar: TopNavBar(),
+            appBar: MessageNavBar(),
             body: Center(
               child: Padding(
                 padding: EdgeInsets.all(24),
@@ -28,8 +21,7 @@ class HomePage extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(FirebaseAuth.instance.currentUser!.uid.toString()),
-                      SignOutButton(),
+                      Text("Message Screen Placeholder"),
                     ]),
               ),
             )));
