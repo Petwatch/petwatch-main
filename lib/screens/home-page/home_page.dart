ae3f0dae7079d46840d235f6de46d98512e7b9a8
Roman Fedoruk <rfedoruk@students.kennesaw.edu>
2022-10-31 17:37:08 -0400

    Feature: home page (WIP)
    Started working on displaying the rest of the information on the card, Date is now formatted if the card is an info post.

--- lib/screens/home-page/home_page.dart
+++ lib/screens/home-page/home_page.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
+import 'package:intl/intl.dart';
 import 'package:petwatch/queries/get_home_page.dart';
 import 'package:petwatch/screens/auth_gate.dart';
 import 'package:petwatch/screens/sign-up/personal_info.dart';
@@ -20,13 +21,59 @@ class HomePage extends StatelessWidget {
   // final BuildContext context;
 
   Widget singlePost(BuildContext context, Map<String, dynamic> post) {
+    // debugPrint(post.toString());
+    // var datePosted =
+    //     new DateTime.fromMicrosecondsSinceEpoch(post['postedTime']);
+    final infoPostDateFormat = new DateFormat('MMMd');
+    final timestamp = post['postedTime'] as Timestamp;
+    var datePosted =
+        DateTime.fromMillisecondsSinceEpoch(timestamp.millisecondsSinceEpoch);
+    // debugPrint(post['postedTime'].toString());
+    // debugPrint(DateFormat.ABBR_MONTH_DAY(timestamp));
+    // debugPrint(infoPostDateFormat.format(datePosted));
+
+    var formattedDate = infoPostDateFormat.format(datePosted);
+
     return GestureDetector(
         onTap: (() {}),
         child: Padding(
-            padding: EdgeInsets.all(24),
-            child: Card(
-              elevation: 5,
-              child: Column(children: [Text(post['title'])]),
+            padding: EdgeInsets.only(top: 24),
+            child: FractionallySizedBox(
+              widthFactor: .95,
+              child: Card(
+                  elevation: 2,
+                  child: IntrinsicHeight(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Row(children: [
+                            CircleAvatar(
+                              // radius: 75,
+                              backgroundColor: Colors.white,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.zero,
+                                child: Image.asset(
+                                  'assets/images/petwatch_logo.png',
+                                ),
+                              ),
+                            ),
+                            Text(post['postedBy']['name']),
+                            Container(
+                              child: VerticalDivider(
+                                width: 20,
+                                thickness: 1,
+                                indent: 20,
+                                endIndent: 0,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            Text(formattedDate),
+                          ]),
+                        )
+                      ],
+                    ),
+                  )),
             )));
   }
 
@@ -34,9 +81,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<UserModel>(builder: (context, value, child) {
       List<Widget> postList = [];
-      value.posts.forEach((post) {
+      for (var post in value.posts) {
         postList.add(singlePost(context, post));
-      });
+      }
 
       return GestureDetector(
           onTap: () {},
@@ -44,13 +91,14 @@ class HomePage extends StatelessWidget {
             appBar: TopNavBar(),
             body: value.postsLoading
                 ? Center(child: CircularProgressIndicator())
-                : Padding(
-                    padding: EdgeInsets.all(24),
+                : Center(
+                    child: Padding(
+                    padding: EdgeInsets.all(0),
                     child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                        // crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [...postList]),
-                  ),
+                  )),
           ));
     });
   }
